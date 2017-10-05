880e92659847bf6e72c9961c9e5a1c6b6608170f
JonatasFAlves <alves.jnts@gmail.com>
2017-10-05 12:30:43 -0300

    Reorganizing files and commits

--- esp8266/final.ino
+++ esp8266/final.ino
@@ -5,74 +5,49 @@ extern "C" {
 #include "user_interface.h"
 }
 
-const char* ssid     = "MPEG-STI";
-const char* password = "w1f1mpeg";
-const char servername[] = "10.250."; // remote server we will connect to
-
-float voltageRead;
-float temperature;
-
-int highTempCont = 0;
-
-String temperatureString;
-
 WiFiClient client;
 
-void setup() {
-    Serial.begin(115200);
-    Serial.println("Connecting");
+const char* ssid     = "MPEG-STI";
+const char* senha = "w1f1mpeg";
+const char servidor[] = "camaleao.museu-goeldi.br";
+const char port = 80;
 
+String stringTemperatura;
+
+float voltagemMedida;
+float temperaturaMedida;
+
+int contadorAltaTemp = 0;
+int contadorBaixaTemp = 0;
+int mediaTemperatura = 23;
+int temperaturaDeAlerta = 23;
+
+void setup() {
     //Inicia pino do led como saída
     pinMode(D1, OUTPUT);
+    WiFi.begin(ssid, senha);
 
-    WiFi.begin(ssid, password);
-
-    testConnection();
-    
-    Serial.println("Connected");
+    testarConexao();
 
     WiFi.mode(WIFI_STA);
     wifi_set_sleep_type(LIGHT_SLEEP_T);
 }
 
 void loop() {    
-  
-    testConnection();
-
-    readSensor();
-
-    Serial.print( "The temperature is: " );
-    Serial.println( temperature );
-    
-
-    if( temperature >= 20.00 ){
-        highTempCont += 1;
-        Serial.print( highTempCont );
-        if ( highTempCont >= 3 ){
-            Serial.println( "Enviando informações por email" );
-            sendDataToServer("temp > 25. It worked!");
-            highTempCont = 0;
-        }
-    }
-    else{
-        highTempCont = 0;
-    }
-
-    temperatureString = String(temperature);
-    sendDataToServer(temperatureString);
-
-    Serial.println("Indo dormir por 30 secs");
-
-    //Espera 10 segundos
-    delay(30000);
+    testarConexao();
+    lerSensor();
+    trataTemperatura(temperaturaMedida);
+    //Espera 60 segundos
+    delay(60000);
 }
 
-void sendDataToServer(String temp) {
+//Conecta ao servidor e chama o script php para enviar email
+void enviarEmail(String nivel, String temp) {
     String result;
-    if (client.connect(servername, 80)) {  //starts client connection, checks for connection
-        Serial.println("connected");
-        client.println("GET /send-email/email.php?temperature="+temp+" HTTP/1.1"); //Send data
-        client.println("Host: 10.250");
+    if (client.connect(servidor, port)) {  //starts client connection, checks for connection
+        Serial.println("conectado");
+        client.println("GET /send-email/email-"+nivel+".php?temperatura="+temp+" HTTP/1.1"); //Send data
+        client.println("Host: camaleao.museu-goeldi.br");
         client.println("Connection: close");  //close 1.1 persistent connection  
         client.println(); //end of get request
     } else {
@@ -90,27 +65,50 @@ void sendDataToServer(String temp) {
     Serial.println(result);
 }
 
-void readSensor() {
-    voltageRead = analogRead(A0);
-    temperature = voltageRead/3.3;
+void lerSensor() {
+    voltagemMedida = analogRead(A0);
+    temperaturaMedida = ((voltagemMedida*3.03)/1024)/0.01;
 }
 
 /* Testa conexão. Caso não consiga conectar ao wifi,
    acende o led e espera o estabelecimento de conexão */
-void testConnection(){
-    Serial.println("Testando conexão");
-
+void testarConexao() {
     if ( WiFi.status() != WL_CONNECTED ){
         digitalWrite(D1, HIGH);
         
         while ( WiFi.status() != WL_CONNECTED) {
-            Serial.print("Attempting to connect to network, SSID: ");
-            Serial.println(ssid);
-
             //Espera 5 segundos antes de tentar outra vez
             delay(5000);
         }
     }
-
     digitalWrite(D1, LOW);
+}
+
+void trataTemperatura(float tempMedida) {
+    //Envia um email a cada aumento de um grau na temperatura.
+    if( tempMedida >= mediaTemperatura ){
+        contadorAltaTemp += 1;
+        if ( contadorAltaTemp >= 3 ){
+            stringTemperatura = String(temperaturaMedida);
+            enviarEmail("aumentou", stringTemperatura);
+            mediaTemperatura += 1;
+            contadorAltaTemp = 0;
+        }
+    } else{
+        contadorAltaTemp = 0;
+    }
+
+    /* Caso a temperatura meça menos dois graus em relação a última temperatura de alerta,
+    envia um email informando que a temperatura está se estabilizando. */
+    if( tempMedida <= (mediaTemperatura-2) && mediaTemperatura > temperaturaDeAlerta ){
+        contadorBaixaTemp += 1;
+        if ( contadorBaixaTemp >= 5 ){
+            stringTemperatura = String(temperaturaMedida);
+            enviarEmail("diminuiu", stringTemperatura);
+            mediaTemperatura -= 1;
+            contadorBaixaTemp = 0;
+        }
+    } else{
+        contadorBaixaTemp = 0;
+    }
 }
