e8efad704ed9601d4eab7cd24b8da97e27316ce6
JonatasFAlves <alves.jnts@gmail.com>
2017-11-07 01:37:51 -0300

    Adicionado script base servidor/cliente para o arduino

---
+++ arduino/webserver.ino
@@ -0,0 +1,94 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 0, 17);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+EthernetClient client;
+
+long lastReadingTime = 0;
+char searchServer[] = "www.google.com"; 
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+
+void loop() {
+  // check if already passed 5 seconds from the last read.
+  if (millis() - lastReadingTime > 5000) {
+    tryConnection();
+    lastReadingTime = millis();
+  }
+
+  listenForEthernetClients();
+}
+
+void listenForEthernetClients(){
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+         // send a standard http response header
+         client.println("HTTP/1.1 200 OK");
+         client.println("Content-Type: text/html");
+          // Control air conditioner
+          Serial.println("Execute command");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(100);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+
+void tryConnection(){
+  // if you get a connection, report back via serial:
+  if (client.connect(searchServer, 80)) {
+    Serial.println("connected");
+    // Make a HTTP request:
+    client.println("GET /search?q=arduino HTTP/1.1");
+    client.println("Host: www.google.com");
+    client.println("Connection: close");
+    client.println();
+  } else {
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+}
