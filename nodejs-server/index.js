b05cae0164a8fd117984fa8d684878d3e82aab47
JonatasFAlves <alves.jnts@gmail.com>
2017-11-08 01:53:07 -0300

    Adicionado exemplos básicos do firebase

--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -3,18 +3,29 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require('path/to/serviceAccountKey.json');
 
-admin.initializeApp({
-    credential: firebase.credential.cert(serviceAccount),
-    databaseURL: 'https://<PROJECT_ID>.firebaseio.com'
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: 'https://<PROJECT_ID>.firebaseio.com'
 });
 
+// AC commands
+var locationRef = admin.database().ref('ac/');
+locationRef.on('child_changed', function (data) {
+    var status = data.key;
+    var on = data.val();
+    console.log(status + ": " + on);
+});
+
+// api
 var app = express();
 
-app.get('/:name', function (req, res) {
-    console.log('USER ' +req.params.name);
+app.get('/:temperature', function (req, res) {
+    // var temperature = req.params.temperature;
+    admin.database().ref('/').update({
+        temperature: req.params.temperature
+    });
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
