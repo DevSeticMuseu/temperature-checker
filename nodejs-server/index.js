1ae286f38828d106ebc649561c6474fa5681172c
JonatasFAlves <alves.jnts@gmail.com>
2017-11-09 20:59:00 -0300

    index.js: Estrutura do arquivo organizada

--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -1,22 +1,21 @@
 "use strict";
 
+var http = require('http');
 var express = require('express');
 var firebase = require("firebase-admin");
 
 var serviceAccount = require('path/to/serviceAccountKey.json');
 
 firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: 'https://<PROJECT_ID>.firebaseio.com'
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: 'https://<PROJECT_ID>.firebaseio.com'
 });
+// End initialize
 
-// AC commands. Get information about ac status
-var locationRef = firebase.database().ref('ac/');
-locationRef.on('child_changed', function (data) {
-    var ac = data.key;
-    var status = data.val();
-    console.log(ac + ": " + status);
-});
+
+var postRequest = function () {
+
+}
 
 var getDate = function () {
     var date = new Date();
@@ -32,17 +31,18 @@ var getDate = function () {
     var hour = date.getHours();
     hour = (hour < 10 ? "0" : "") + hour;
 
-    var min  = date.getMinutes();
+    var min = date.getMinutes();
     min = (min < 10 ? "0" : "") + min;
 
     var fullDate = day + "-" + month + "-" + year;
     var fullTime = hour + "h" + min;
 
     return {
-        "day" : fullDate,
-        "time" : fullTime
+        "day": fullDate,
+        "time": fullTime
     }
 }
+
 
 // api
 var app = express();
@@ -55,6 +55,14 @@ app.post('/sensor/api/:temperature', function (req, res) {
     });
 });
 
+// AC commands. Get information about ac status
+var locationRef = firebase.database().ref('ac/');
+locationRef.on('child_changed', function (data) {
+    var ac = data.key;
+    var status = data.val();
+    console.log(ac + ": " + status);
+});
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
