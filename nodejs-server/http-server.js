a94485a64367d4f032df129797eaefd090a8e28b
JonatasFAlves <alves.jnts@gmail.com>
2017-11-21 23:26:32 -0300

    Separado o método "getDate" para um arquivo próprio
    Esta modificação visa facilitar a reusabilidade deste bloco de código por outras partes da aplicação

--- nodejs-server/http-server.js
+++ nodejs-server/http-server.js
@@ -21,29 +21,3 @@ http.createServer(function (req, res) {
 
     res.end();
 }).listen(3000);
-
-const getDate = function () {
-    var date = new Date();
-
-    var year = date.getFullYear();
-
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-
-    var day = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-
-    var min = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-
-    var fullDate = day + "-" + month + "-" + year;
-    var fullTime = hour + "h" + min;
-
-    return {
-        "day": fullDate,
-        "time": fullTime
-    }
-}
---
+++ nodejs-server/time.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const getDate = function () {
+    var date = new Date();
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec = date.getSeconds();
+
+    var fullDate = day + "-" + month + "-" + year;
+    var fullTime = hour + min + sec;
+
+    return {
+        "day": fullDate,
+        "time": fullTime
+    }
+}
